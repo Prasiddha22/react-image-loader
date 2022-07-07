30a7212cdae4c008a4e8998208654f326f073dbd
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-07 21:43:12 +0545

    FIX: package json

--- tsdx.config.js
+++ tsdx.config.js
@@ -2,7 +2,8 @@ const postcss = require('rollup-plugin-postcss');
 
 module.exports = {
     rollup(config) {
-        config.output = [{ dir: './dist', format: 'cjs' }];
+        config.output = { dir: './dist', format: 'cjs' };
+        config.build = { dir: './dist', format: 'cjs' };
         config.plugins.push(postcss());
         return config;
     },
